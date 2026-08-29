5ab5374dd69154d534fc87ebf2844eb61c05793b
UjwalGupta1307 <ujwalburgupally7@gmail.com>
2026-08-29 22:35:54 +0530

    Add transaction domain model

---
+++ src/main/java/com/example/transactionstarter/transaction/entity/Transaction.java
@@ -0,0 +1,80 @@
+package com.example.transactionstarter.transaction.entity;
+
+import com.example.transactionstarter.transaction.enums.Currency;
+import com.example.transactionstarter.transaction.enums.TransactionStatus;
+import com.example.transactionstarter.transaction.enums.TransactionType;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+
+    @Id
+    private String transactionId;
+
+    private String customerId;
+
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    private Currency currency;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionType transactionType;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionStatus transactionStatus;
+
+    public Transaction() {
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public TransactionType getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(TransactionType transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public TransactionStatus getTransactionStatus() {
+        return transactionStatus;
+    }
+
+    public void setTransactionStatus(TransactionStatus transactionStatus) {
+        this.transactionStatus = transactionStatus;
+    }
+}
---
+++ src/main/java/com/example/transactionstarter/transaction/enums/Currency.java
@@ -0,0 +1,5 @@
+package com.example.transactionstarter.transaction.enums;
+
+public enum Currency {
+    INR
+}
